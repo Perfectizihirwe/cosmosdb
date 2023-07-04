508fb58abfd0c2a246490cd4a86aa9c5a853f192
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-07-04 11:32:24 +0200

    Add lunar class library

---
+++ Lunar.Domain/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Lunar.Domain
+{
+    public class BaseEntity
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Discriminator { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
